a8f2b6f213643b7c77a557f5decc5c407fc3c23b
Jonathan <jonjova1992@hotmail.com>
2021-08-04 11:12:16 -0600

    Crud modal y wizard alumno

--- application/views/Layout/Footer.php
+++ application/views/Layout/Footer.php
@@ -1,11 +1,11 @@
         </div>
         <!--agregar los div aqui termina contenido de la página  -->
 
-    </div>
-    <!--es nesesarios este div de la clase wrapper -->
+ </div>
+ <!--es nesesarios este div de la clase wrapper -->
 
-<div class="overlay">
-</div>
+ <div class="overlay">
+ </div>
 </body>
 <!--tambien es necesario este div para overlay  -->
 
@@ -35,10 +35,22 @@
 <script type="text/javascript" src="<?php echo base_url();?>assets/scripts/Proyecto/ValidaProyecto.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>assets/scripts/Proyecto/CrudProyecto.js"></script>
 
+<!--CRUD GRUPO ALUMNO-->
+<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/GrupoAlumno/CrudGrupoAlumno.js"></script>
+
+<!--CRUD ALUMNO-->
+<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/Alumno/crudAlumno.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/Alumno/ValidaAlumno.js"></script>
+
 <!-- ALERTAS JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" ></script>
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/sweetalert2.js"></script>	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" ></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/sweetalert2.js"></script> 
+
+<!--Select2 -->
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/select2.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" ></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-select.js');?>"></script>
 
 <!-- CHART JS -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -46,25 +58,12 @@
 <!--custom-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
+<!--sidebar-->
+<script type="text/javascript" src="<?php echo base_url('assets/js/sidebar.js');?>"></script>
 
-<!--Importante base_url() es un complementa a la url que hagamos en ajax -->	
+<!--Importante base_url() es un complementa a la url que hagamos en ajax -->    
 <script type="text/javascript">
-	var url = '<?php echo base_url();?>';
-	
-  $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
-    $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').removeClass('active');
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-     $('#sidebar').toggleClass('active');
- });
-});
+    var url = '<?php echo base_url();?>';
 </script>
 
 </html>
